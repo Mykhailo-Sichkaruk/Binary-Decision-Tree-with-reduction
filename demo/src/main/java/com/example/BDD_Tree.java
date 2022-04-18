f82f97d4cb06934c269f4f24b7e7c26ead16c301
Mykhailo Sichkaruk <misha0510@gmail.com>
2022-04-18 19:39:34 +0200

    DNF_generator() added

--- demo/src/main/java/com/example/BDD_Tree.java
+++ demo/src/main/java/com/example/BDD_Tree.java
@@ -15,15 +15,8 @@ public class BDD_Tree {
         // Creating 1 level of Tree
         this.order = order;
         BDD_Root = new BDD_Node(b_function, String.valueOf(order.charAt(0)));
-
-        //
     }
 
-    public static int b_function_hashCode(String b_func) {
-        int result = 0;
-        for (int i = 0; i < b_func.length(); i++)
-            b_func.charAt(i);
-        return result;
-    }
+
 
 }
